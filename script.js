7075caf18ce88b1dabcf2657632e98b997b157b4
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-02-19 11:01:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+  const modal = document.getElementById("myModal");
+        const openModal = document.getElementById("openModal");
+        const closeModal = document.querySelector(".close-modal");
+        
+        openModal.addEventListener("click", () => {
+            modal.style.display = "flex";
+        });
+        
+        closeModal.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+        
+        window.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+            }
+        });
